ffcb3258b8c7cac101177f4d48ccccc745134113
Yulia-Safonova-85 <109626421+Yulia-Safonova-85@users.noreply.github.com>
2023-06-12 19:11:12 +0300

    update code - using nodemailer

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,45 +1,25 @@
-const Mailjet = require("node-mailjet");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const {MJ_APIKEY_PUBLIC, MJ_APIKEY_PRIVATE, MJ_SENDER_EMAIL} = process.env;
+const {UKR_NET_EMAIL, UKR_NET_PASSWORD} = process.env;
 
-const mailjet = new Mailjet({
-    apiKey: MJ_APIKEY_PUBLIC,
-    apiSecret: MJ_APIKEY_PRIVATE
-  });
-
-  const data = {
-     to:"",
-     subject:"Verification email sent",
-     html:"<h3>Dear Konsyella, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!</h3><br />May the delivery force be with you!"
-
+const nodemailerConfig = {
+  host: "smtp.ukr.net",
+  port: 465,
+  secure: true,
+  auth: {
+    user: UKR_NET_EMAIL,
+    pass: UKR_NET_PASSWORD,
   }
 
-  const sendEmail = async (data) => {
-    await mailjet.post('send', { version: 'v3.1' })
-        .request({
-          Messages: [
-            {
-              From: {
-                Email: MJ_SENDER_EMAIL,
-                // Name: "Mailjet Pilot"
-              },
-              To: [
-                {
-                  Email: data.to,
-                  Name: ""
-                }
-              ],
-              Subject: data.subject,
-              TextPart: "Dear friend, welcome to Mailjet! May the delivery force be with you!",
-              HTMLPart: data.html,
-            
-            }
-          ]
-        })
+};
+const transport = nodemailer.createTransport(nodemailerConfig);
 
-return true;
-  }
+const sendEmail = async(data) => {
+  const email = {...data, from: UKR_NET_EMAIL};
+  await transport.sendMail(email);
+  return true;
+}
 
-module.exports = sendEmail;
-  
+module.exports = sendEmail; 
+
